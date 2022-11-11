47b67e0638a3afb2ba4fc66da336a0f5a766ebf8
Zherui Yang <yangzherui2001@foxmail.com>
2022-11-11 13:34:49 +0800

    add multi-thread test

--- examples/xmake.lua
+++ examples/xmake.lua
@@ -1,9 +1,19 @@
 target('ui_only_world')
   add_files('ui_only_world/**.cpp')
   add_deps('acg_visualizer')
+
 target('nbody')
   add_files('nbody/**.cpp')
   add_deps('acg_visualizer', 'acg_core')
+
 target('free-fall')
   add_files('free_fall/**.cpp')
   add_deps('acg_visualizer', 'acg_core')
+target_end()
+
+add_requires('openmp')
+target('test_eigen_mp')
+  add_files('test_eigen_mp/**.cpp')
+  add_deps('acg_core')
+  add_packages("openmp")
+ 
